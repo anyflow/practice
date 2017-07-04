550db7a5060bcced2aa66e9178996de88ccbae23
Park Hyunjeong <anyflow@MacBook.local>
2017-07-05 08:05:47 +0900

    Add RootToLeafpaths.cpp

--- cpp/RootToLeafPaths.cpp
+++ cpp/RootToLeafPaths.cpp
@@ -1,5 +1,5 @@
 // question :
-// http://practice.geeksforgeeks.org/problems/leftmost-and-rightmost-nodes-of-binary-tree/1
+// http://practice.geeksforgeeks.org/problems/root-to-leaf-paths/1
 
 #include <algorithm>
 #include <iostream>
@@ -20,21 +20,13 @@ struct Node {
 
 using Path = vector<Node*>;
 
-int sum(Path* path) {
-  int ret = 0;
-  for (auto item : *path) {
-    ret += item->data;
-  }
-
-  return ret;
-}
-
-void bfs(int k, Node* startNode, vector<Path*>& sums) {
+void printPaths(Node* root) {
   auto paths = map<Node*, Path*>();
   auto parents = map<Node*, Node*>();
+  auto leafPaths = vector<Path*>();
 
   auto q = queue<Node*>();
-  q.push(startNode);
+  q.push(root);
 
   while (!q.empty()) {
     auto node = q.front();
@@ -47,8 +39,8 @@ void bfs(int k, Node* startNode, vector<Path*>& sums) {
 
     paths[node] = path;
 
-    if (sum(path) == k) {
-      sums.push_back(path);
+    if (!node->left && !node->right) {
+      leafPaths.push_back(path);
     }
 
     if (node->left) {
@@ -62,37 +54,13 @@ void bfs(int k, Node* startNode, vector<Path*>& sums) {
 
     q.pop();
   }
-}
 
-int printCount(Node* root, int k) {
-  auto sums = vector<Path*>();
-
-  auto q = queue<Node*>();
-  q.push(root);
-
-  while (!q.empty()) {
-    root = q.front();
-    bfs(k, root, sums);
-
-    if (root->left) {
-      q.push(root->left);
+  for (auto path : leafPaths) {
+    for (auto i : *path) {
+      cout << i->data << " ";
     }
-    if (root->right) {
-      q.push(root->right);
-    }
-
-    q.pop();
+    cout << "# ";
   }
-
-  for (auto item : sums) {
-    cout << "size : " << item->size() << " | ";
-
-    for (auto node : *item) {
-      cout << node->data << " ";
-    }
-    cout << endl;
-  }
-  return sums.size();
 }
 
 void insert(Node* root, int n1, int n2, char lr) {
@@ -153,13 +121,12 @@ Node* parse(string& target) {
 }
 
 int main(int argc, char* argv[]) {
-  auto testcases = map<int, string>();
-  testcases[4] = "1 3 L 3 2 L 3 -1 R -1 1 R";
-  testcases[3] = "0 1 L 0 2 R 1 3 L 1 4 R 2 5 L 2 6 R";
+  auto testcases =
+      vector<string>{"1 2 R 1 3 L", "10 20 L 10 30 R 20 40 L 20 60 R"};
 
   for (auto item : testcases) {
-    cout << "k=" << item.first << "|tree:" << item.second << endl;
-    cout << printCount(parse(item.second), item.first) << endl;
+    printPaths(parse(item));
+    cout << endl;
   }
 
   return 0;
