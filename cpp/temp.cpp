d6076279e951027f5460e2463322be929c623493
Park Hyunjeong <anyflow@MacBook.local>
2017-07-06 07:12:04 +0900

    update

--- cpp/temp.cpp
+++ cpp/temp.cpp
@@ -5,23 +5,4 @@
 
 using namespace std;
 
-auto t = [](auto a, auto b) { return a + b; };
-
-struct Node {
-  int data;
-};
-
-struct NodeEx : public Node {
-  Node* parent;
-  // NodeEx(Node* node) : data(node->data), left(node->left), right(node->right)
-  // {}
-  NodeEx() {}
-  NodeEx(Node* node, Node* parent) : Node{node->data}, parent(parent) {}
-};
-
-int main() {
-  auto node = new Node{11};
-  auto n1 = new NodeEx(node, nullptr);
-
-  cout << node->data << endl;
-}
+int main() {}
--- javascript/bitwise.js
+++ javascript/bitwise.js
@@ -14,9 +14,7 @@ function clearBitsMSBthroughI(num, i) {
   return num & mask;
 }
 
-function insert(M, N, i, j) {
-
-}
-
 console.log(clearBitsMSBthroughI(15, 3).toString(2));
+
+console.log((4 >>> 0).toString(2));
 process.exit(0);
