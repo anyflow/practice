1d673369fc87589ea67e934db26d168c7692f0c9
Park Hyunjeong <anyflow@MacBook.local>
2017-07-09 14:17:03 +0900

    GreyCodeSequence.cpp

---
+++ cpp/GreyCodeSequence.cpp
@@ -0,0 +1,49 @@
+// http://practice.geeksforgeeks.org/problems/generate-grey-code-sequences/1
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void get(vector<int>& prev, int pos) {
+  int prefix = 1 << (pos - 1);
+
+  vector<int> news;
+  for (int i = prev.size() - 1; i >= 0; --i) {
+    news.push_back(prefix + prev[i]);
+  }
+
+  prev.insert(prev.end(), news.begin(), news.end());
+}
+
+string bin(int N, int num) {
+  auto ret = string(N, '0');
+
+  int mask = 1 << N;
+
+  int pos = 0;
+  while (mask >>= 1) {
+    ret[pos++] = num & mask ? '1' : '0';
+  }
+
+  return ret;
+}
+
+int main() {
+
+  auto list = vector<int>();
+
+  list.push_back(0b0);
+  list.push_back(0b1);
+
+  for (int i = 2; i <= 4; ++i) {
+    get(list, i);
+  }
+
+  for (auto i : list) {
+    cout << bin(4, i) << " ";
+  }
+
+  return 0;
+}
--- cpp/temp.cpp
+++ cpp/temp.cpp
@@ -1,11 +1,22 @@
 #include <bitset>
-#include <climits>
 #include <iostream>
-
 using namespace std;
 
 int main() {
-  uint n = 229;
+  unsigned t = 5, bit = 0;
 
+  unsigned ret = 0;
+
+  for (int i = 1; i < 32; ++i) {
+    ret |= t & 1;
+
+    t >>= 1;
+    ret <<= 1;
+  }
+
+  cout << ret << endl;
+
+  cout << bitset<32>(ret).to_string() << endl;
+  cout << bitset<4>("1010").to_ulong() << endl;
   return 0;
 }
