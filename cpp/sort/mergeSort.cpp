26722169fba1da0338d2b277711d69eae14d9fba
Park Hyunjeong <anyflow@gmail.com>
2017-07-11 18:12:29 +0900

    update

---
+++ cpp/sort/mergeSort.cpp
@@ -0,0 +1,37 @@
+#include <chrono>
+#include <climits>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+void sort(vector<int>& arr) {
+  for (int i = 0; i < arr.size(); ++i) {
+  }
+}
+
+int main() {
+  vector<int> input = {
+      3,    1,    5,   9,    0,   10,  5,    32,  -1,  -20,  239, 583,
+      6,    3,    54,  53,   38,  34,  653,  75,  345, 7576, 45,  27,
+      4564, 8765, 24,  1,    435, 564, 2435, 654, 986, 345,  234, 3745,
+      44,   33,   22,  77,   88,  99,  11,   22,  4,   5,    6,   8,
+      3,    4,    6,   7443, 221, 4,   3475, 543, 769, 435,  145, 456,
+      2463, 8789, 324, 1242, 34,  136, 678,  986, 44,  5463, 234, 789};
+
+  auto before = system_clock::now();
+  sort(input);
+  auto after = system_clock::now();
+
+  auto elapsed = duration_cast<microseconds>(after - before).count();
+  cout << "elapsed time : " << elapsed << endl;
+
+  for (auto& i : input) {
+    cout << i << " ";
+  }
+
+  cout << endl;
+  return 0;
+}
