f5858518d95f872a79e9284124a37e9f7c0cdf37
박현정/책임연구원/클라우드센터 서비스플랫폼담당 서비스개발팀(hyunjeong.park@lge.com) <hyunjeong.park@lge.com>
2018-11-24 01:23:16 +0900

    Fix cpp compile error on launch.json

---
+++ cpp/basic/numberOfOccurrence.cpp
@@ -0,0 +1,32 @@
+// https://practice.geeksforgeeks.org/problems/replace-all-0-with-5-in-an-input-integer/1
+
+#include "../common/practice.h"
+
+using namespace std;
+
+int convertFive(int n) {
+  int ret = 0;
+  int placer = 1;
+
+  while (n >= 1) {
+    int digit = n % 10;
+
+    if (digit == 0) {
+      digit = 5;
+    }
+
+    ret += digit * placer;
+
+    placer *= 10;
+
+    n = (int)(n / 10);
+  }
+
+  return ret;
+}
+
+int main() {
+  cout << convertFive(190104) << endl;
+
+  return 0;
+}
