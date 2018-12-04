5380b41245ca9cc0fb060c49bb302e57838d7ee8
Park Hyunjeog <hyunjeong.park@lge.com>
2018-12-04 09:15:16 +0900

    Complete rotateBits.cpp

--- cpp/level.1.basic/rotateBits.cpp
+++ cpp/level.1.basic/rotateBits.cpp
@@ -4,6 +4,41 @@
 
 using namespace std;
 
-// TODO
+using bit16 = short unsigned int;
 
-int main() { return 0; }
+void printRotate(bit16 N, int D) {
+  bit16 left, right, leftBit, rightBit;
+
+  bit16 leftMask = 1 << 15;
+  bit16 rightMask = 1;
+
+  left = right = N;
+
+  for (int i = 0; i < D; ++i) {
+    leftBit = leftMask & left;
+    left <<= 1;
+
+    left += leftBit ? 1 : 0;
+
+    rightBit = rightMask & right;
+    right >>= 1;
+
+    right += rightBit ? leftMask : 0;
+  }
+
+  cout << to_string(left) << endl << to_string(right) << endl;
+}
+
+int main() {
+  int T;
+
+  cin >> T;
+  while (T--) {
+    bit16 N;
+    int D;
+
+    cin >> N >> D;
+
+    printRotate(N, D);
+  }
+}
