b34ea380dc2171e90448990f9f4082805a4a3ab1
Park Hyunjeong <anyflow@gmail.com>
2019-01-23 14:05:01 +0900

    Start max-value.cpp

---
+++ cpp/level.2.easy/max-value.cpp
@@ -0,0 +1,13 @@
+// https://practice.geeksforgeeks.org/problems/max-value/0/?ref=self
+// FAILED
+
+#include "../lib/practice.h"
+
+using namespace std;
+using namespace anyflow;
+
+int main(int argc, char* argv[]) {
+  // TODO
+
+  return 0;
+}
