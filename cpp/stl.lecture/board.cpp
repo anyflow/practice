876f4b93b764274a15e5088f76ae01056f17c1b4
Park Hyunjeong <anyflow@gmail.com>
2019-10-28 13:08:26 +0900

    Add board.cpp in stl.lecture

---
+++ cpp/stl.lecture/board.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+int main() {
+  bitset<8> b1 = 0b11110000;
+
+  b1.set(); // to 1 for all
+  b1.reset(); // to 0 for all
+
+  b1.set(1); // 0000 0010
+  b1[2] = 1; // 0000 0110
+  b1[0].flip(); // 0000 0111
+
+  cout << b1 << endl;
+
+  string s = b1.to_string();
+  unsigned long n = b1.to_ulong();
+
+  cout << s << endl;
+  cout << n << endl;
+
+  return 0;
+}
