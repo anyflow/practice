735b3dea1eb7a88285ef40df145e60691e5fbf1f
Park Hyunjeong <anyflow@gmail.com>
2022-12-15 02:53:07 +0900

    Add first Go file and leetcode c++ file

---
+++ algorithm/cpp/leetcode/RotateArray.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1182/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void rotate(vector<int>& nums, int k) {
+    if (k == 0) {
+      return;
+    }
+
+    // solution #1
+    //  time complexity : O(N)
+    //  space complexity : O(1)
+    //  for(int i=0; i<k; ++i) {
+    //      nums.insert(nums.begin(), nums.back());
+    //      nums.pop_back();
+    //  }
+
+    // solution #2
+    //  time complexity : O(1)
+    //  space complexity : O(N)
+    k = k % nums.size();
+
+    nums.insert(nums.begin(), nums.end() - k, nums.end());
+    nums.erase(nums.end() - k, nums.end());
+  }
+};
+
+void print(vector<int>& items) {
+  cout << "[";
+  for (auto i = 0; i < items.size(); ++i) {
+    cout << items[i];
+    if (i != items.size() - 1) {
+      cout << ", ";
+    }
+  }
+
+  cout << "]" << endl;
+}
+
+int main() {
+  vector<int> nums = {1, 2, 3};
+
+  cout << "input: " << endl;
+  print(nums);
+
+  Solution().rotate(nums, 1);
+
+  cout << "output: " << endl;
+  print(nums);
+
+  return 0;
+}
