a7d1e01939af5686c48bfa47edb2868b04051a8e
Park Hyunjeong <anyflow@gmail.com>
2023-02-12 01:48:15 +0900

    Refactoring

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-<<<<<<< Updated upstream
 func sum(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
@@ -24,25 +23,4 @@ func main() {
 	x, y := <-c, <-c // receive from c
 
 	fmt.Println(x, y, x+y)
-=======
-type Point struct {
-	x, y int
-}
-
-func main() {
-	r1 := Point{0, 1}
-
-	r2 := r1
-
-	p1 := &r1
-
-	r2.x, r2.y = 2, 4
-
-	fmt.Println(r1, r2)
-
-	p1.x, p1.y = 3, 6
-	fmt.Println(r1)
-
-	fmt.Println(p1)
->>>>>>> Stashed changes
 }
